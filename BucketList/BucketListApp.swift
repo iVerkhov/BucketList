29b52f2ce34ed48cc8c6c5b9c1c7f59471c0f663
iVerkhov <vrkhv@iCloud.com>
2023-09-26 20:05:42 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Игорь Верхов on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
